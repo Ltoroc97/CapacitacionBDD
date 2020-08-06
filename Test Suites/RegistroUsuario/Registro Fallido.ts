f6c7d65811568b569e2afd7f2dabdde5f5f4aa27
Ltoroc <Ltoroc@P-LTOROC.intergrupo.com>
2020-08-06 18:59:52 -0500

    Se sube un escenaario de ejecución 2

---
+++ Test Suites/RegistroUsuario/Registro Fallido.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Registro Fallido</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>724d82c6-8313-4ae6-bd16-9b272722b7d0</testSuiteGuid>
+   <testCaseLink>
+      <guid>dc2bd511-24fc-409f-8acc-fae88b9fd9d3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RegistroUsuario/RegistroUsuario</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
